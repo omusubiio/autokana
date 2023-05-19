c9344e5e3a7f046139980da86626a12e7d230dd3
Omusubi <omusubi91@gmail.com>
2023-05-19 12:13:29 +0900

    track henkan removal

--- autokana.js
+++ autokana.js
@@ -3,40 +3,88 @@ var AutoKana = function(options) {
 
   var pattern = options.pattern || /[\u3041-\u3094]/
   
-  function transcript(furigana, previous, current) {
-    var common = 0
-    for (var m = Math.max(previous.length, current.length), i = 0; i < m; i++) {
-      if (previous.charAt(i) !== current.charAt(i)) {
-        break 
-      }
-      common++
-    }
-
-    var removed = previous.substring(common)
-    var appended = current.substring(common)
-
-    if (removed.length === 1 && appended.length === 1
-      && removed.match(pattern) && appended.match(pattern)) {
-      // mobile style input method swaps the last letter
-      return furigana.substring(0, furigana.length - 1) + appended
-    }
-
-    for (var i = 0; i < appended.length; i++) {
-      var letter = appended.charAt(i)
-      if (letter.match(pattern)) {
-        furigana += letter 
-      } else {
-        break 
-      }
-    }
-
-    return furigana 
-  }
 
   function newHandler(source, target) {
     var previous = source.get() 
 
+    var checkpoints = []
+    
+    function addCheckpoint(s, t) {
+      checkpoints.push({
+        source: s || '',
+        target: t || '',
+      })
+    }
+    
+    function transcript(furigana, previous, current) {
+      function grow(newFurigana) {
+        addCheckpoint(current, newFurigana)
+        return newFurigana
+      }
+
+      var common = 0
+      for (var m = Math.max(previous.length, current.length), i = 0; i < m; i++) {
+        if (previous.charAt(i) !== current.charAt(i)) {
+          break 
+        }
+        common++
+      }
+  
+      var removed = previous.substring(common)
+      var appended = current.substring(common)
+  
+      if (removed.length === 1 && appended.length === 1
+        && removed.match(pattern) && appended.match(pattern)) {
+        // mobile style input method swaps the last letter
+        return grow(furigana.substring(0, furigana.length - 1) + appended)
+      }
+  
+      // if (appended.length === 0 
+      //   && removed.length === 1 && removed.match(pattern) 
+      //   && furigana.length > 0 && furigana.charAt(furigana.length - 1) === removed) {
+      //   // backspace 
+      //   return grow(furigana.substring(0, furigana.length - 1))
+      // }
+
+      if (appended.length === 0) {
+        // backspace
+        for (var i = checkpoints.length; i > 0; i--) {
+          var cp = checkpoints[i - 1]
+          if (!current.startsWith(cp.source))
+            break 
+          
+          if (cp.source === current) {
+            if (furigana.startsWith(cp.target)) {
+              furigana = cp.target
+              break
+            }
+          }
+        }
+
+        // shrink checkpoints 
+        checkpoints = checkpoints.filter(function(cp) {
+          return current.startsWith(cp.source)
+        })
+
+        return furigana 
+      }
+  
+      for (var i = 0; i < appended.length; i++) {
+        var letter = appended.charAt(i)
+        if (letter.match(pattern)) {
+          furigana += letter 
+        } else {
+          break 
+        }
+      }
+  
+      return grow(furigana)
+    }
+
+    addCheckpoint(source.get(), target.get())
+
     return function() {
+      // console.log(checkpoints)
       var current = source.get()
       // console.log(previous, ' => ', current)
 
@@ -61,7 +109,7 @@ var AutoKana = function(options) {
 
   function subscribe(sourceEl, targetEl) {
     var handle = newHandler(ofInputElement(sourceEl), ofInputElement(targetEl))
-    sourceEl.addEventListener('keyup', function() {
+    sourceEl.addEventListener('input', function() {
       handle()
     })
   }
